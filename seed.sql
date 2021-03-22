baec2cac9b28887c3c8b393ea82710ef17ba2130
Gina Vargas <gina.vargas89@outlook.com>
2021-03-22 16:39:22 -0700

    adding seeds

--- seed.sql
+++ seed.sql
@@ -1,4 +1,24 @@
 -- Adding seed to pre-populate database --
+-- department seeds --
+-- Creates new rows containing data in all named columns --
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineer");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+
+
+
+
+
+
+
+
+
+
 INSERT INTO department (id, name) VALUES (1, 'Engineering');
 
 INSERT INTO role (title, salary, departmentID) VALUES ("Lead Engineer", 150000, 1);
